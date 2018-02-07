5b9ad2f80309d6af432eca25e4754e4532f61994
HanBing Guo <772918850@qq.com>
2018-02-07 15:17:16 +0800

    update demo_scan with bugs

--- 09_demo_scan/demo_scan.cu
+++ 09_demo_scan/demo_scan.cu
@@ -2,6 +2,7 @@
 #include <numeric>
 #include <stdlib.h>
 #include <stdio.h>
+#include<thrust/scan.h>
 
 /*
 Somethings so confuse me, why i can't get same correct result every time.
@@ -75,6 +76,55 @@ __global__ void shared_hillis_steele_scan_backward(float * d_out, float * d_in, 
 	d_out[idx] = sdata[idx];
 }
 
+/*
+This kernel will get correct result when array size is power of 2
+*/
+ __global__ void blelloch_exclusive_scan(float *g_odata, float *g_idata, int n)
+{
+	extern __shared__ float temp[];// allocated on invocation
+
+
+	int thid = threadIdx.x;
+	int offset = 1;
+	temp[thid] = g_idata[thid];	
+
+	for (int d = n / 2; d > 0; d /= 2) // build sum in place up the tree
+	{
+		__syncthreads();
+
+		if (thid < d)
+		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
+
+			temp[bi] += temp[ai];
+		}
+		offset <<= 1; //multiply by 2 implemented as bitwise operation
+	}
+	
+	if (thid == 0) { temp[n - 1] = 0; } // clear the last element
+
+	for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
+	{
+		offset >>= 1;
+		__syncthreads();
+
+		if (thid < d)
+		{
+		int ai = offset*(2*thid+1)-1;
+		int bi = offset*(2*thid+2)-1;
+
+		float t = temp[ai];
+		temp[ai] = temp[bi];
+		temp[bi] += t;
+		}
+	}
+
+	__syncthreads();
+	g_odata[thid] = temp[thid];
+	
+} 
+
 int main(int argc, char ** argv) {
 	const int ARRAY_SIZE = 1025;
 	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
@@ -102,11 +152,14 @@ int main(int argc, char ** argv) {
 	//hillis_steele_scan_forward<<<numBlock, maxThreadPerBlock>>>(d_out, d_in, ARRAY_SIZE);
 	//hillis_steele_scan_backward<<<numBlock, maxThreadPerBlock>>>(d_out, d_in);
 	//shared_hillis_steele_scan_forward<<<numBlock, maxThreadPerBlock, maxThreadPerBlock  * sizeof(float)>>>(d_out, d_in, ARRAY_SIZE);
-	shared_hillis_steele_scan_backward<<<numBlock, maxThreadPerBlock, maxThreadPerBlock  * sizeof(float)>>>(d_out, d_in, ARRAY_SIZE);
-
-	// copy back the result array to the CPU
-	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
-
+	//shared_hillis_steele_scan_backward<<<numBlock, maxThreadPerBlock, maxThreadPerBlock  * sizeof(float)>>>(d_out, d_in, ARRAY_SIZE);
+	//blelloch_exclusive_scan<<<numBlock, maxThreadPerBlock, maxThreadPerBlock * sizeof(float)>>>(d_out, d_in, ARRAY_SIZE);
+	
+	
+	thrust::inclusive_scan(h_in, h_in + ARRAY_SIZE, h_out);
+	
+	//copy back the result array to the CPU
+	//cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 	// print out the resulting array
 	for (int i =0; i < ARRAY_SIZE; i++) {
 		printf("%f", h_out[i]);
